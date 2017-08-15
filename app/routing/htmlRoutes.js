fcf0dac78ba025021846af5a4d276fbf9b7dd735
dacup72 <dacup72@gmail.com>
2017-08-15 17:22:09 -0600

    added new assets files and updated server.js

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,12 +1,21 @@
 var path = require('path');
+var express = require("express");
 
 module.exports = function(req, res) {
+
+  app.use(express.static(__dirname + "/../public"));
+
+  //Path for homepage
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
+
+  //Path for survey page
   app.get("/survey", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
+
+  //Default path to homepage
   app.use(function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
--- server.js
+++ server.js
@@ -1,19 +1,20 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var path = require("path");
 
 var app = express();
 
-var PORT = process.env.PORT || 3000;
+var PORT = process.env.PORT || 8080;
 
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.text());
-app.use(bodyParser.json({ type: "application/vnd.api+json" }));
+app.use(bodyParser.json({ type: 'application/*+json' }));
+app.use(bodyParser.raw({ type: 'application/vnd.custom-type' }));
+app.use(bodyParser.text({ type: 'text/html' }));
 
-app.get("./app/public/home.html");
+// app.get("./app/public/home.html");
 
-require("./app/routing/apiRoutes").app;
-require("./app/routing/htmlRoutes").app;
+require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
