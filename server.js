070acf520a41a678d62d54e828b119205ec4bd9d
dacup72 <dacup72@gmail.com>
2017-08-15 16:46:19 -0600

    Added html

--- server.js
+++ server.js
@@ -12,8 +12,8 @@ app.use(bodyParser.json({ type: "application/vnd.api+json" }));
 
 app.get("./app/public/home.html");
 
-require("./app/routing/apiRoutes");
-require("./app/routing/htmlRoutes");
+require("./app/routing/apiRoutes").app;
+require("./app/routing/htmlRoutes").app;
 
 app.listen(PORT, function() {
   console.log("App listening on PORT: " + PORT);
