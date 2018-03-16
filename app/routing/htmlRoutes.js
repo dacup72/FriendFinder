a13bd62efa69f3fda1ec4d9547ec42ea69eba370
dacup72 <dacup72@gmail.com>
2018-03-15 19:39:57 -0600

    Refactored html routes

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,18 +2,12 @@ var path = require('path');
 
 module.exports = function(app) {
 
-  //Path for homepage
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
-  });
-
-  //Path for survey page
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/survey.html"));
+    res.sendFile(path.join(__dirname, "/../public/survey.html"));
   });
 
-  //Default path to homepage
-  app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "../public/home.html"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "/../public/home.html"));
   });
+
 };
