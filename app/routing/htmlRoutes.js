7504d152bc21b9c7386ebee4d5d1563047ab5d28
dacup72 <dacup72@gmail.com>
2018-03-15 18:21:57 -0600

    Updated html

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -3,20 +3,20 @@ var express = require("express");
 
 module.exports = function(app) {
 
-  app.use(express.static(__dirname + "/../public"));
+  // app.use(express.static(__dirname + "/../public"));
 
   //Path for homepage
   app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 
   //Path for survey page
   app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/survey.html"));
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
   });
 
   //Default path to homepage
   app.use(function(req, res) {
-    res.sendFile(path.join(__dirname, "/../public/home.html"));
+    res.sendFile(path.join(__dirname, "../public/home.html"));
   });
 };
