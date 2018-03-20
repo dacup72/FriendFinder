171c1c745e1ff7a17ca46912f6bebacb9d2fb04a
dacup72 <dacup72@gmail.com>
2018-03-19 18:49:01 -0600

    added data object constructor

--- app/public/javascript/friends.js
+++ app/public/javascript/friends.js
@@ -21,5 +21,26 @@ $("#submit").on("click", function(event) {
     return isValid;
   }
 
-  
+  // If all required fields are filled
+  if (validateForm()) {
+    // Create an object for the user"s data
+    var userData = {
+      name: $("#name").val(),
+      photo: $("#photo").val(),
+      scores: [
+        $("#q1").val(),
+        $("#q2").val(),
+        $("#q3").val(),
+        $("#q4").val(),
+        $("#q5").val(),
+        $("#q6").val(),
+        $("#q7").val(),
+        $("#q8").val(),
+        $("#q9").val(),
+        $("#q10").val()
+      ]
+    };
+
+    
+  }
 });
