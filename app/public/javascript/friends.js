4a1a219160a84feb125c0a8edec92bc0e44806f1
dacup72 <dacup72@gmail.com>
2018-03-22 15:53:33 -0600

    adding logic to check for chosen css properties

--- app/public/javascript/friends.js
+++ app/public/javascript/friends.js
@@ -1,5 +1,23 @@
+// Config for chosen css
+var config = {
+  ".chosen-select": {},
+  ".chosen-select-deselect": {
+    allow_single_deselect: true
+  },
+  ".chosen-select-no-single": {
+    disable_search_threshold: 10
+  },
+  ".chosen-select-no-results": {
+    no_results_text: "Oops, nothing found!"
+  },
+  ".chosen-select-width": {
+    width: "95%"
+  }
+};
 
 
+
+// Submit button
 $("#submit").on("click", function(event) {
   event.preventDefault();
 
